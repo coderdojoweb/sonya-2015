f37207c81dec915cf2636c1a8d0216412ffb8b44
drewcovi <drew.covi@me.com>
2015-11-07 15:24:00 -0600

    new site

--- js/main.js
+++ js/main.js
@@ -1,11 +1,6 @@
-$('.flip')
-  .click(function(){
-    location.href="https://www.google.com/webhp?sourceid=chrome-instant&ion=1&espv=2&es_th=1&ie=UTF-8#q=zerg%20rush&es_th=1"
-  })
-  .hover(function(){
-    // if(Math.round(Math.random())){
-      $(this).css("transform", 'translate('+Math.random()*200+'px,'+Math.random()*200+'px)')
-    // }else{
+$(".special").click(function(){
 
-    // }
-  })
+alert("this horse is brown!");
+$("body").css("background","yellow");
+
+});
